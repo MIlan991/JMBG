f3b1b23ae6ca7110e1a0f8366a7f71a74da984cf
MIlan991 <81048118+MIlan991@users.noreply.github.com>
2024-02-13 19:23:23 +0100

    Add files via upload

--- JMBG/src/Cubes/MainClass.java
+++ JMBG/src/Cubes/MainClass.java
@@ -1,17 +1,17 @@
 package Cubes;
 
-
 import java.time.Year;
 import java.util.Scanner;
+import java.util.Date; 
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int currentYear = Year.now().getValue();
-		boolean badInput = true;
-
-
+		Date d = new Date();
+		int currentMonth = (d.getMonth()+1);
+		byte option = 0;
 			do {
 				String jmbg;
 				
@@ -23,10 +23,11 @@ public class MainClass {
 				String k;
 				
 				System.out.println("Unesite Vas JMBG: ");
-				jmbg = scanner.nextLine();
-				System.out.println("Vas jmbg je: " + jmbg);
-				System.out.println(" ");
+				jmbg = scanner.next();
 				
+					System.out.println("Vas jmbg je: " + jmbg);
+					System.out.println("");
+					
 				dd = jmbg.substring(0, 2);
 				int ddInt = Integer.parseInt(dd);
 				
@@ -45,8 +46,53 @@ public class MainClass {
 				k = jmbg.substring(12, 13);
 				int kInt = Integer.parseInt(k);
 				
+				int l = 0;
+				
+				String l1;
+				String l2;
+				String l3;
+				String l4;
+				String l5;
+				String l6;
+				String l7;
+				String l8;
+				String l9;
+				String l10;
+				String l11;
+				String l12;
+				String l13;
+				
+				l1 = jmbg.substring(0,1);
+				int l1Int = Integer.parseInt(l1);
+				l2 = jmbg.substring(1,2);
+				int l2Int = Integer.parseInt(l2);
+				l3 = jmbg.substring(2,3);
+				int l3Int = Integer.parseInt(l3);
+				l4 = jmbg.substring(3,4);
+				int l4Int = Integer.parseInt(l4);
+				l5 = jmbg.substring(4,5);
+				int l5Int = Integer.parseInt(l5);
+				l6 = jmbg.substring(5,6);
+				int l6Int = Integer.parseInt(l6);
+				l7 = jmbg.substring(6,7);
+				int l7Int = Integer.parseInt(l7);
+				l8 = jmbg.substring(7,8);
+				int l8Int = Integer.parseInt(l8);
+				l9 = jmbg.substring(8,9);
+				int l9Int = Integer.parseInt(l9);
+				l10 = jmbg.substring(9,10);
+				int l10Int = Integer.parseInt(l10);
+				l11 = jmbg.substring(10,11);
+				int l11Int = Integer.parseInt(l11);
+				l12 = jmbg.substring(11,12);
+				int l12Int = Integer.parseInt(l12);
+				l13 = jmbg.substring(12,13);
+				int l13Int = Integer.parseInt(l13);
+				
 				//OUTPUT DAYS
-				if( (mmInt == 02 && (ddInt > 28 || ddInt < 1)) && (mmInt == 02 && gggInt % 100 != 0 && gggInt % 4 == 0 && (ddInt > 29 || ddInt < 1)) ) {
+				if( (gggInt % 100 != 0 && gggInt % 4 == 0) && mmInt == 02 && (ddInt > 29 || ddInt < 1) ) {
+					System.out.println("Uneli ste lose prva dva broja");
+				} else if((gggInt % 100 != 0 && gggInt % 4 != 0) && (mmInt == 02) && (ddInt > 28 || ddInt < 1) ) {
 					System.out.println("Uneli ste lose prva dva broja");
 				} else if((mmInt == 04 || mmInt == 06 || mmInt == 9 || mmInt == 11) && (ddInt > 30 || ddInt < 1)){
 					System.out.println("Uneli ste lose prva dva broja");
@@ -54,7 +100,6 @@ public class MainClass {
 					System.out.println("Uneli ste lose prva dva broja");
 				} else {
 					System.out.println("Dan rodjenja je: " + dd);
-					badInput = false;
 				}
 				
 				//OUTPUT MONTHS
@@ -97,12 +142,12 @@ public class MainClass {
 				} else {
 					System.out.println("Uneli ste lose 3. i 4. broj.");
 				}
-				
+				//                         currentMonth
 				//OUTPUT YEAR
 				if(gggInt > 900) {
 					gggInt = 1000 + gggInt;
 					System.out.println("Godina rodjenja je: " + gggInt);
-				} else if((2000 + gggInt) > currentYear) {
+				} else if( (2000 + gggInt) > currentYear || mmInt > currentMonth) {
 					gggInt = 0;
 					System.out.println("Uneli ste pogresno 5, 6, ili 7.");
 				} else if(gggInt < 100){
@@ -141,20 +186,23 @@ public class MainClass {
 				}
 				
 				//OUTPUT L NUMBER
-				// L = 11 – (( 7*(A+E) + 6*(B+Ž) + 5*(V+Z) + 4*(G+I) + 3*(D+J) + 2*(Đ+K) ) % 11) kInt
-
+				l = 11 - (( 7*(l1Int+l7Int) + 6*(l2Int+l8Int) + 5*(l3Int+l9Int) + 4*(l4Int+l10Int) + 3*(l5Int+l11Int) + 2*(l6Int+l12Int) ) % 11);
 				
+				if( l == kInt && (l > 1 && l < 10)) {
+					l = kInt;
+				} else if(l >= 10 && l != kInt) {
+					l = 0;
+				}else if(l != kInt){
+					System.out.println("Kontrolni broj se ne poklapa. Lose ste uneli JMBG");
+				}
 				
-				
-				
-			}while(badInput = true);
-				
-
-		
+				System.out.println("Kontrolni broj je: " + l);
+				System.out.println("");
+				System.out.print("Ukoliko zelite opet da validirate JMBG, pritisnite (1). Ako ne, pritisnite bilo koji drugi broj! ");
+				option = scanner.nextByte();
+			}while(option == 1);		
 			
+			scanner.close();
 	}
-
 }
-//else if((mmInt == 02 && gggInt % 100 != 0 && gggInt % 4 == 0) && (ddInt > 29 || ddInt < 1)) {
-//System.out.println("Prestupna godina je. Februar ima 29 dana");
-//}
+// 1512993857412
